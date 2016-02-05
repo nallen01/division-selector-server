dc27e4742372a3bd7f488b9d5d3e8ad3d8831ec1
Nathan Allen <nathan.allen.nz@gmail.com>
2016-02-05 18:52:42 +1300

    Initial commit with placeholder main file

---
+++ src/me/nallen/divisionselector/server/DivisionSelectorServer.java
@@ -0,0 +1,27 @@
+package me.nallen.divisionselector.server;
+
+public class DivisionSelectorServer {
+	//public static DivisionData divisionData;
+	//DivisionGui gui;
+	//TcpServer tcpServer;
+	
+	public static void main(String[] args) {
+		new DivisionSelectorServer();
+	}
+	
+	public DivisionSelectorServer() {
+		init();
+	}
+	
+	public void init() {
+		/*divisionData = new DivisionData();
+		divisionData.clear();
+		
+		// Start the TCP Server
+		tcpServer = new TcpServer();
+		tcpServer.run();
+		
+		// Start the GUI
+		gui = new DivisionGui();*/
+	}
+}
